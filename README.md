8479228a0429a1c6ffb2053cd44de85c12e3dc3a
Darko Gjorgjijoski <dg@darkog.com>
2024-03-16 21:55:20 +0100

    Init

---
+++ README.md
@@ -0,0 +1,59 @@
+# Digital License Manager C++ Example
+
+This is simple example that covers all the required operations to successfully show, activate, validate and deactivate a license key.
+
+### Getting started
+
+To get started using this example:
+
+1. Follow the section 1 in **Requirements**.
+2. Set up the API connection in main.cpp with your api details
+3. Set up SampleKey variable in main.cpp with your key
+
+Run
+
+```
+cmake build .
+bash dlm_examples
+```
+
+Sample output:
+
+```
+====== Digital License Manager Example ======
+Checking License Key
+- License is K2CH-GIG1-IGC3-2N1K valid.
+Activating token
+- License K2CH-GIG1-IGC3-2N1K activated...
+- Token a12fdde7d50aee0123d7741c351c55a8c45cc129 SET.
+Validating token
+- Token a12fdde7d50aee0123d7741c351c55a8c45cc129 is valid...
+- License K2CH-GIG1-IGC3-2N1K is valid.
+Deactivating token
+- Token a12fdde7d50aee0123d7741c351c55a8c45cc129 deactivated...
+```
+
+### Requirements
+
+#### 1. libcpr 
+
+The libcpr library is required, to install follow the guide bellow:
+
+Note: Action required.
+
+```
+git clone https://github.com/libcpr/cpr.git
+cd cpr && mkdir build && cd build
+cmake .. -DCPR_USE_SYSTEM_CURL=ON
+cmake --build . --parallel
+sudo cmake --install .
+```
+
+
+#### 2. dlmclient
+
+The dlmclient classes are required. However in this project those are included as git submodule.
+
+#### 3. RapidJSON
+
+The rapidjson json parser library is required. In this project the library is included as git submodule.
---
+++ main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+
+#include "lib/dlm-cpp/include/client.hpp"
+#include "lib/rapidjson/include/rapidjson/document.h"
+
+
+void StoreTokenInStorage(std::string token) {
+    // Serves as an example of when you need to store the token.
+}
+
+void DeleteTokenFromStorge() {
+    // Serves as an example of when you need to delete the token.
+}
+
+int main() {
+
+    std::cout << "====== Digital License Manager Example ======" << std::endl;
+
+    // Initialize the API
+    DigitalLicenseManager::Connection conn = DigitalLicenseManager::create_connection(
+            "http://dlm.test",
+            "v1",
+            "ck_211f37385f51f31cf820f28e1c00cc04ed9690bb",
+            "cs_fe4970b3468b8452bed2853e550bf5e829401eeb"
+    );
+
+    // Declare Response
+    DigitalLicenseManager::Response response;
+
+    // Declare RapidJSON document
+    rapidjson::Document doc;
+
+    // Used variables below
+    std::string SampleKey = "K2CH-GIG1-IGC3-2N1K";
+    std::string token = "";
+
+    // 1. EXAMPLE: /licenses/{key}
+    std::cout << "Checking License Key" << std::endl;
+    response = DigitalLicenseManager::licenses_get(conn, SampleKey);
+    doc.Parse(response.get_contents().c_str());
+    if (doc.HasMember("code") && doc.HasMember("message")) {
+        std::cout << doc["message"].GetString();
+    } else if (doc.HasMember("success") && doc["success"].GetBool()) {
+        auto is_expired = doc["data"]["is_expired"].GetBool();
+        std::string license_key = doc["data"]["license_key"].GetString();
+        if (is_expired) {
+            std::cout << "- License is " + license_key + " expired." << std::endl;
+        } else {
+            std::cout << "- License is " + license_key + " valid." << std::endl;
+        }
+    } else {
+        std::cout << "- Invalid response" << std::endl;
+    }
+
+    // 2. EXAMPLE: /licenses/activate/{license_key}
+    std::cout << "Activating token" << std::endl;
+    response = DigitalLicenseManager::licenses_activate(conn, SampleKey, "Device X");
+    doc.Parse(response.get_contents().c_str());
+    if (doc.HasMember("code") && doc.HasMember("message")) {
+        std::cout << doc["message"].GetString() << std::endl;
+    } else if (doc.HasMember("success") && doc["success"].GetBool()) {
+        token = doc["data"]["token"].GetString(); // Store the token somewhere securely.
+        StoreTokenInStorage(token); // Example...
+        std::cout << "- License " + SampleKey + " activated..." << std::endl;
+        std::cout << "- Token " + token + " SET." << std::endl;
+    } else {
+        std::cout << "- Invalid response" << std::endl;
+    }
+
+    // 3. EXAMPLE: /licenses/validate/{token}
+    std::cout << "Validating token" << std::endl;
+    if (!token.empty()) {
+        response = DigitalLicenseManager::licenses_validate(conn, token);
+        doc.Parse(response.get_contents().c_str());
+        if (doc.HasMember("code") && doc.HasMember("message")) {
+            std::cout << doc["message"].GetString() << std::endl;
+        } else if (doc.HasMember("success") && doc["success"].GetBool()) {
+            token = doc["data"]["token"].GetString(); // Store the token somewhere securely.
+            StoreTokenInStorage(token); // Example...
+            std::cout << "- Token " + token + " is valid..." << std::endl;
+            if (doc["data"].HasMember("license")) {
+                bool is_expired = doc["data"]["license"]["is_expired"].GetBool();
+                std::string license_key = doc["data"]["license"]["license_key"].GetString();
+                if (is_expired) {
+                    std::cout << "- License " + license_key + " is expired." << std::endl;
+                } else {
+                    std::cout << "- License " + license_key + " is valid." << std::endl;
+                }
+            } else {
+                std::cout << "- License NOT found." << std::endl;
+            }
+        } else {
+            std::cout << "- Invalid response" << std::endl;
+        }
+    } else {
+        std::cout << "- Token NOT found." << std::endl;
+    }
+
+    // 4. EXAMPLE /licenses/deactivate/{token}
+    std::cout << "Deactivating token" << std::endl;
+    if (!token.empty()) {
+        response = DigitalLicenseManager::licenses_deactivate(conn, token);
+        doc.Parse(response.get_contents().c_str());
+        if (doc.HasMember("code") && doc.HasMember("message")) {
+            std::cout << doc["message"].GetString() << std::endl;
+        } else if (doc.HasMember("success") && doc["success"].GetBool()) {
+            DeleteTokenFromStorge(); // Example...
+            std::cout << "- Token " + token + " deactivated..." << std::endl;
+        } else {
+            std::cout << "- Invalid response" << std::endl;
+        }
+    } else {
+        std::cout << "- Token NOT found." << std::endl;
+    }
+
+    return 0;
+}
